9500f18ac5c8f99df7aad3b1e0c97de07e31a66a
radt <radthoc@gmail.com>
2020-06-27 00:01:24 +0200

    Update controller

--- app/Http/Controllers/TimeController.php
+++ app/Http/Controllers/TimeController.php
@@ -2,11 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Domain\MartianTimeInterface;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 
 class TimeController extends Controller
 {
+    /** @var MartianTimeInterface */
+    private $martianTimeService;
+
+    public function __construct(MartianTimeInterface $martianTimeService)
+    {
+        $this->martianTimeService = $martianTimeService;
+    }
+
     /**
      *
      * @param string $time
@@ -14,8 +23,6 @@ class TimeController extends Controller
      */
     public function __invoke(string $time)
     {
-        return response()->json([
-            'time' => $time,
-        ]);
+        return response()->json([$this->martianTimeService->getMartianTime($time)]);
     }
 }
